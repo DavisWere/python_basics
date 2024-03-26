633ecf290b956324b0ba8d2d1fe35a936e2f5279
DavisWere <devisodhis10@gmail.com>
2024-03-26 23:10:56 +0300

    add file initial.py

---
+++ initial.py
@@ -0,0 +1,39 @@
+"""print("welcome to python class")
+# adding numbers
+print(1+2) # 3"""
+"""num1 = 566778
+num2 =45678
+results = num1 + num2
+print(results)"""
+
+StudentName = "davis"
+Student, Name, grades = "davis", "were", 30
+# datatypes
+#string - str
+# number - int  float complex
+# list   tuple set dictionary bool NoneType
+
+is_male  = True
+age      = 29
+height  = 1.75
+weight  = 65.5
+eye_color= 'Brown'
+
+# print(type(eye_color))
+#list - Lists are used to store multiple items in a single variable.
+MyList= ['bmw', 'audi','toyota','lexus','v8', 'benz',2 ,4,6,8,7,9,0, False, 'hello']
+#print(len(MyList))
+#indexing starts from 0
+MyList[7]= "Mercedes"
+# if 4 in MyList:
+#     print('item is present')
+# else:
+#     print('item not available')
+#print(MyList)
+#print(MyList[1:7])
+#print(type(MyList))
+#user input 
+Name = input("Enter your name: ")
+Age = int(input("How old are you? :"))
+print("My name is  ", Name)
+print("I am ", Age," years old.")
