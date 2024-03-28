20af4cd39237674994c21e1d30e549777cae2a84
DavisWere <devisodhis10@gmail.com>
2024-03-28 23:42:52 +0300

    added datetime

--- textspeech.py
+++ textspeech.py
@@ -1,8 +1,10 @@
 import pyttsx3
+import datetime
 
 engine = pyttsx3.init()
-engine.say("good morning")
-engine.say('hello victor, davis, were, enda numerical')
+engine.say("the time is: "  + str(datetime.datetime.now()))
+engine.setProperty('rate',200) # Speed of the audio (default=150)
+engine.say('hello , ')
 engine.say(input("enter your name: "))
 engine.runAndWait()
 
